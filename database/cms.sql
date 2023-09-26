179504e2c07725fbb613728c55d9e444b1165a4f
Akhilesh <98794660+Saga-Akhilesh@users.noreply.github.com>
2023-09-26 22:52:53 +0530

    Update cms.sql

--- database/cms.sql
+++ database/cms.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 10, 2020 at 04:25 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.28
+-- Host: localhost:3307
+-- Generation Time: Sep 26, 2023 at 07:20 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -44,10 +43,10 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `category`, `link`, `type`, `order_by`, `is_root`, `parent_id`, `date_created`) VALUES
-(1, 'Navigation 1', 'index.php?page=list&c=navigation_1&cid=c4ca4238a0b923820dcc509a6f75849b', 1, 0, 1, 0, '2020-12-10 11:17:43'),
-(2, 'Navigation 2', 'index.php?page=list&c=navigation_2&cid=c81e728d9d4c2f636f067f89cc14862c', 1, 3, 1, 0, '2020-12-10 11:18:17'),
-(3, 'Sub Navigation 1', 'index.php?page=list&c=sub_navigation_1&cid=eccbc87e4b5ce2fe28308fd9f2a7baf3', 1, 1, 0, 1, '2020-12-10 11:30:42'),
-(4, 'Sub Navigation 2', 'index.php?page=list&c=sub_navigation_2&cid=a87ff679a2f3e71d9181a67b7542122c', 1, 2, 0, 1, '2020-12-10 11:37:02');
+(1, 'Navigation 1', 'index.php?page=list&c=navigation_1&cid=c4ca4238a0b923820dcc509a6f75849b', 1, 0, 1, 0, '2023-09-25 11:17:43'),
+(2, 'Navigation 2', 'index.php?page=list&c=navigation_2&cid=c81e728d9d4c2f636f067f89cc14862c', 1, 3, 1, 0, '2023-09-25 11:18:17'),
+(3, 'Sub Navigation 1', 'index.php?page=list&c=sub_navigation_1&cid=eccbc87e4b5ce2fe28308fd9f2a7baf3', 1, 1, 0, 1, '2023-09-25 11:30:42'),
+(4, 'Sub Navigation 2', 'index.php?page=list&c=sub_navigation_2&cid=a87ff679a2f3e71d9181a67b7542122c', 1, 2, 0, 1, '2023-09-25 11:37:02');
 
 -- --------------------------------------------------------
 
@@ -69,7 +68,7 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`comment_id`, `user_id`, `comment`, `content_id`, `date_created`, `date_modified`) VALUES
-(1, 1, '<p>sample</p>', 2, '2020-12-10 21:43:20', NULL);
+(1, 1, '<p>sample</p>', 2, '2023-09-25 21:43:20', NULL);
 
 -- --------------------------------------------------------
 
@@ -95,8 +94,8 @@ CREATE TABLE `contents` (
 --
 
 INSERT INTO `contents` (`id`, `title`, `category_id`, `author_id`, `meta_keywords`, `meta_description`, `link`, `banner_img`, `date_created`, `date_modified`) VALUES
-(2, 'Sample Content', 3, 1, 'Tag 1,Tag 2,Tag 3,asdasd', 'SAMPLELorem ipsum dolor sit amet, consectetur adipiscing elit. Duis volutpat est imperdiet lobortis varius. Donec posuere, tortor quis maximus fringilla, tellus mauris porta lacus, vestibulum consequat lacus massa rutrum dui. Donec sagittis mauris semper nisi condimentum, non feugiat nisl vestibulum', '1_Sample-Content.html', '1607589840_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-10 16:44:04', '2020-12-10 20:25:30'),
-(3, 'Sample 101', 4, 1, 'tags', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in felis viverra, tincidunt lacus eget, malesuada metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut rhoncus est sed nisl rhoncus, et aliquam ante auctor. Aliquam egestas facilisis ex ', '3_Sample-101.html', '1607603820_1605604020_123065338-creative-dark-coding-background-with-text-programming-and-future-concept-3d-rendering.jpg', '2020-12-10 20:37:32', '2020-12-10 20:41:25');
+(4, 'Bharat Intern', 1, 1, '', 'Connects students with internships:\r\n                      Bharat Intern connects students with internships in a\r\n                      variety of fields. This gives students the opportunity to\r\n                      gain experience, skills, and a network. Gives students experience, skills, and a ne', '4_Bharat-Intern.html', '', '2023-09-26 22:17:55', '2023-09-26 22:17:55'),
+(5, 'Bharat Intern2', 2, 1, '', 'Bharat Intern2', '5_Bharat-Intern2.html', '', '2023-09-26 22:22:20', '2023-09-26 22:22:20');
 
 -- --------------------------------------------------------
 
@@ -142,8 +141,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `avatar`, `date_created`) VALUES
-(1, 'Administrator', '', 'admin@admin.com', '0192023a7bbd73250516f069df18b500', 1, '1607590140_avatar.jpg', '2020-11-26 10:57:04'),
-(2, 'John', 'Smith', 'jsmith@sample.com', '1254737c076cf867dc53d60a0364f38e', 3, 'no-image-available.png', '2020-12-10 22:25:38');
+(1, 'Administrator', '', 'admin@admin.com', '0192023a7bbd73250516f069df18b500', 1, '1607590140_avatar.jpg', '2023-11-25 08:57:04'),
+(3, '123', '123', '123@gmail.com', '202cb962ac59075b964b07152d234b70', 3, 'no-image-available.png', '2023-09-26 17:46:23'),
+(4, '1234', '1234', '1234@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 3, 'no-image-available.png', '2023-09-26 22:21:29');
 
 --
 -- Indexes for dumped tables
@@ -199,7 +199,7 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `contents`
 --
 ALTER TABLE `contents`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `system_settings`
@@ -211,7 +211,7 @@ ALTER TABLE `system_settings`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
